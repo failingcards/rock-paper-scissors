cd1653ece21aa00dc3324b08a4d585cbb2da3307
Ernesto <216224551+failingcards@users.noreply.github.com>
2025-08-20 13:48:11 -0400

    Finished rock paper scissors sequence (forgot to commit :/)

---
+++ script.js
@@ -0,0 +1,55 @@
+function playGame() {
+    function getHumanChoice() {
+        let humanChoice = prompt("Pick your choice");
+        return humanChoice;
+    };
+    
+    function getComputerChoice() {
+        let computerChoice = Math.floor(Math.random() * 3);
+        if (computerChoice === 0) {
+            return "scissors";
+        } else if (computerChoice === 1) {
+            return "paper";
+        } else {
+            return "rock";
+        }
+    };
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        humanChoice = getHumanChoice();
+        computerChoice = getComputerChoice();
+        humanChoice = humanChoice.toLowerCase();
+        if (humanChoice === computerChoice) {
+            console.log("Tie!");
+        } else if (
+            (humanChoice === "rock" && computerChoice === "scissors") ||
+            (humanChoice === "paper" && computerChoice === "rock") ||
+            (humanChoice === "scissors" && computerChoice === "paper")
+        ) {
+            console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+            humanScore++;
+        } else {
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+            computerScore++;
+        }
+    };
+
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+
+    if (computerScore > humanScore) {
+        console.log(`Computer wins! Score: ${computerScore} - ${humanScore}.`);
+    } else if (humanScore > computerScore) {
+        console.log(`You win! Score: ${humanScore} - ${computerScore}.`);
+    } else {
+        console.log("It's a tie!")
+    };
+};
+
+playGame();
